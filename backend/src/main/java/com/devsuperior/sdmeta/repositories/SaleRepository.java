51f166827bb97a5f5270fdf5b011ecf23f607a2b
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2022-07-17 11:48:44 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/sdmeta/repositories/SaleRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.sdmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.sdmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	
+
+}
